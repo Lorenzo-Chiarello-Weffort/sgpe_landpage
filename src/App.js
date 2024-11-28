8ec4acf6a8809b1c9ac9e14eaa1840d2780af77a
Lorenzo <lorenzo.weffort@aluno.ifsp.edu.br>
2024-11-27 22:37:40 -0300

    Teste inicial de pegar link por URL

--- src/App.js
+++ src/App.js
@@ -1,20 +1,47 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaSpinner } from 'react-icons/fa';
 import './App.css';
 
 function App() {
 
-  // Link
-  const link = "https://www.google.com";
+  const [link, setLink] = useState('');
+  const prefix = 'https://';
 
   useEffect(() => {
-    window.location.href = link;
+
+    const fileUrl = 'https://pub.microbin.eu/raw/bat-swan-tiger';
+
+    const fetchLink = async () => {
+      try {
+        const response = await fetch(fileUrl);
+        if (response.ok) {
+          console.log("respnse ok");
+          const linkText = await response.text();
+          setLink(linkText);
+        } else {
+          console.error('Erro ao carregar a URL.');
+        }
+      } catch (error) {
+        console.error('Erro de rede:', error);
+      }
+    };
+
+    fetchLink();
   }, []);
+
+  useEffect(() => {
+    if (link) {
+      // window.location.href = prefix + link;
+      console.log(link);
+    } else {
+      console.error("Link não encontrado.");
+    }
+  }, [link]);
 
   return (
     <div className="App">
       <div className="loading-container">
-        <p>Carregado...</p>
+        <p>Carregando...</p>
         <FaSpinner className="spinner" size={40} style={{ animation: 'spin 2s linear infinite' }} />
       </div>
     </div>
